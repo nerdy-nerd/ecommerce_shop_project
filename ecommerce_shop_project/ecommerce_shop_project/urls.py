4d27dcbf692bb8d6014c107de127ae693a4c1665
Dominik <swierzek@gmail.com>
2019-03-18 20:33:04 +0100

    Fix urlpatterns in main urls

--- ecommerce_shop_project/ecommerce_shop_project/urls.py
+++ ecommerce_shop_project/ecommerce_shop_project/urls.py
@@ -6,10 +6,12 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls, name="admin"),
-    path("", RedirectView.as_view(url="admin/", permanent=False), name="index"),
+    #path("", RedirectView.as_view(url="admin/", permanent=False), name="index"),
+    path("", include("shop.urls"))
 ]
 
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
