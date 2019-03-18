220e5efc3fccb30c625c9af00bf1c7acbb5df52d
Dominik <swierzek@gmail.com>
2019-03-18 18:33:32 +0100

    Add product_list view

--- ecommerce_shop_project/shop/views.py
+++ ecommerce_shop_project/shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . models import Product
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.filter(available__exct=True)
+    context = {"products": products}
+    return render(request, 'shop/product_list.html', context=context)
