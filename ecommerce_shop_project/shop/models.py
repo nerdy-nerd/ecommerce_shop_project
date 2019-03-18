68bb18024f196c23b4c80341127ca742273e4349
Dominik <swierzek@gmail.com>
2019-03-18 15:48:05 +0100

    Add Product model

---
+++ ecommerce_shop_project/shop/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=150, db_index=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_add=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
