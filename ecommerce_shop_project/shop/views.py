8aba68750efc140360e7af0a341b948322b06aad
Floydu1994 <kacper.floydu@gmail.com>
2019-03-18 20:02:31 +0100

    bezuzyteczny commit

--- ecommerce_shop_project/shop/views.py
+++ ecommerce_shop_project/shop/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-from .models import Product
-from django.http import HttpResponse
+from django.db import models
+from django.utils import timezone
 
 
-def product_list(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "shop/product_list.html", context=context)
+class Product(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=150, db_index=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
