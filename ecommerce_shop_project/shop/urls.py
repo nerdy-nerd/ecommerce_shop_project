c248e3027c5e825d99ab2d51b7c1f2034099b523
Floydu1994 <kacper.floydu@gmail.com>
2019-03-20 20:17:40 +0100

    format correction

--- ecommerce_shop_project/shop/urls.py
+++ ecommerce_shop_project/shop/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.product_list, name="product_list"),
-               path("product/<int:pk>/", views.product_detail, name="product_detail"), ]
+urlpatterns = [
+    path("", views.product_list, name="product_list"),
+    path("product/<int:pk>/", views.product_detail, name="product_detail"),
+]
