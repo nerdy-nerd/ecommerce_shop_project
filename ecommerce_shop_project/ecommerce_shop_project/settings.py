3de106ec8c2d354536a192a16ddcd991a66edb43
Dominik <swierzek@gmail.com>
2019-03-20 18:39:24 +0100

    Add product detail

--- ecommerce_shop_project/ecommerce_shop_project/settings.py
+++ ecommerce_shop_project/ecommerce_shop_project/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "debug_toolbar",
-
     "django.contrib.staticfiles",
     "shop",
 ]
--- ecommerce_shop_project/shop/views.py
+++ ecommerce_shop_project/shop/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Product
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 
 def product_list(request):
     products = Product.objects.all()
     context = {"products": products}
     return render(request, "shop/product_list.html", context=context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "shop/product_detail.html", {"product": product})
