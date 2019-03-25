a36d6f675ba505ce0a3bea1f8128b21ed9f234fa
Dominik <swierzek@gmail.com>
2019-03-25 09:04:23 +0100

    Style website using bootstrap template

--- ecommerce_shop_project/account/forms.py
+++ ecommerce_shop_project/account/forms.py
@@ -7,6 +7,11 @@ from django import forms
 class AccountRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
