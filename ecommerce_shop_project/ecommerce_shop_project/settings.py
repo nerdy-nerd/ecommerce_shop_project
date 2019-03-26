79bff33cb26b4b0598699d7e5474e2527c8ae92b
Dominik <swierzek@gmail.com>
2019-03-26 21:53:32 +0100

    Add view, url, template for products in category
    fix login redirect to index.html
    
    rebase with current master

--- ecommerce_shop_project/ecommerce_shop_project/settings.py
+++ ecommerce_shop_project/ecommerce_shop_project/settings.py
@@ -122,6 +122,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
-LOGIN_REDIRECT_URL = 'shop:product_list'
+LOGIN_REDIRECT_URL = "shop:index"
--- ecommerce_shop_project/shop/urls.py
+++ ecommerce_shop_project/shop/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.product_list, name="product_list"),
+    path("", views.IndexView.as_view(), name="index"),
     path("product/<int:pk>/", views.product_detail, name="product_detail"),
+    path(
+        "category/<str:name>/",
+        views.CategoryProductView.as_view(),
+        name="category_product",
+    ),
 ]
