1a0bd2dc9cbdd36c195818c3e711fe7c5b14a419
Dominik <swierzek@gmail.com>
2019-03-29 14:22:32 +0100

    Add form and edit detail view for displaying users comments

--- ecommerce_shop_project/shop/models.py
+++ ecommerce_shop_project/shop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -44,7 +45,7 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_add=True)
+    updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
     class Meta:
