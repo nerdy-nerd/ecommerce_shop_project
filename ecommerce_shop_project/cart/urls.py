b04b248fefed458b42fc2f836a0980018565dc4d
Mira Kaminska <rachevamiroslava@gmail.com>
2019-04-07 17:04:33 +0200

    fix of url names

--- ecommerce_shop_project/cart/urls.py
+++ ecommerce_shop_project/cart/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = "cart"
 
 urlpatterns = [
-    url(r"^$", views.cart_detail, name="cart_detail"),
-    url(r"^add/(?P<product_id>\d+)/$", views.cart_add, name="cart_add"),
-    url(r"^remove/(?P<product_id>\d+)/$", views.cart_remove, name="cart_remove"),
+    path("", views.cart_detail, name="cart_detail"),
+    path("add/<product_id>/", views.cart_add, name="cart_add"),
+    path("remove/<product_id>/", views.cart_remove, name="cart_remove"),
 ]
