ebac6a143bcad28604bee11cdde111dbaaa90ebf
Mira Kaminska <rachevamiroslava@gmail.com>
2019-04-07 20:50:43 +0200

    change od dynamic cart

--- ecommerce_shop_project/dynamic_cart/views.py
+++ ecommerce_shop_project/dynamic_cart/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 # from django.views.decorators.http import require_POST
 from cart.cart import Cart
-# from cart.forms import CartAddProductForm
+from cart.forms import CartAddProductForm
 
 
 def cart_data(request):
-    return render(request, "cart_data.html")
+    cart = Cart(request)
+    return render(request, "cart_data.html", {"cart": cart})
+
+
