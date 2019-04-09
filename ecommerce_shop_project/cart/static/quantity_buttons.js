102739dea202910757f4d513e04fe0a689a2a483
Mira Kaminska <rachevamiroslava@gmail.com>
2019-04-09 19:10:41 +0200

    Add js script

---
+++ ecommerce_shop_project/cart/static/quantity_buttons.js
@@ -0,0 +1,9 @@
+$('.add').click(function () {
+		alert("The paragraph was clicked.");
+		}
+);
+$('.sub').click(function () {
+		if this.next().val() > 1 {
+    	if this.next().val() > 1 this.next().val+this.next().val() - 1;
+		}
+});
