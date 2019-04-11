61b410fb8325ef46ea2676cf59634921b6848701
Mira Kaminska <rachevamiroslava@gmail.com>
2019-04-11 15:24:17 +0200

    Add +- buttons to cart detail and product detail views

--- ecommerce_shop_project/cart/static/quantity_buttons.js
+++
@@ -1,9 +0,0 @@
-$('.add').click(function () {
-		alert("The paragraph was clicked.");
-		}
-);
-$('.sub').click(function () {
-		if this.next().val() > 1 {
-    	if this.next().val() > 1 this.next().val+this.next().val() - 1;
-		}
-});
