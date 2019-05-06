7e2c26b27a378db90ba5241f5a5848f3c137b572
Mira Kaminska <rachevamiroslava@gmail.com>
2019-05-06 18:00:02 +0200

    add messages

--- ecommerce_shop_project/orders/views.py
+++ ecommerce_shop_project/orders/views.py
@@ -25,18 +25,5 @@ def order_create(request):
         return render(request, 'orders/order/created.html', {'order': order})
 
     else:
-        if request.user.is_authenticated:
-            data = {
-                "first_name": request.user.first_name,
-                "last_name": request.user.last_name,
-                "email": request.user.email,
-                "address": request.user.street,
-                "postal_code": request.user.code,
-                "city": request.user.city,
-                "country": request.user.country,
-            }
-            form = OrderCreateForm(initial=data)
-        else:
-            form = OrderCreateForm()
-    return render(request, "orders/order/create.html", {"form": form})
-
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
