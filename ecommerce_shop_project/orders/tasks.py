6e7b3f2e82cc26dff927b39613f080e1268acf7a
Dominik <swierzek@gmail.com>
2019-05-10 19:59:49 +0200

    fix email sending

--- ecommerce_shop_project/orders/tasks.py
+++ ecommerce_shop_project/orders/tasks.py
@@ -6,12 +6,10 @@ from .models import Order
 @task
 def order_created(order_id):
     order = Order.objects.get(id=order_id)
-    subject = 'Order nr. {}'.format(order.id)
-    message = 'Dear {},\n\nYou have successfully placed an order.\
-     Your order id is {}.'.format(order.first_name,
-                                  order.id)
-    mail_sent = send_mail(subject,
-                          message,
-                          'admin@myshop.com',
-                          [order.email])
+    subject = "Order nr. {}".format(order.id)
+    message = "Dear {},\n\nYou have successfully placed an order.\
+     Your order id is {}.".format(
+        order.address.name, order.id
+    )
+    mail_sent = send_mail(subject, message, "admin@myshop.com", [order.user.email])
     return mail_sent
