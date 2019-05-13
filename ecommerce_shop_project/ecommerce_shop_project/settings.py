dcef5dab39d1e6bf6d08174821553caa184a954c
Mira Kaminska <rachevamiroslava@gmail.com>
2019-05-13 20:13:54 +0200

    add postgres db

--- ecommerce_shop_project/ecommerce_shop_project/settings.py
+++ ecommerce_shop_project/ecommerce_shop_project/settings.py
@@ -88,16 +88,24 @@ WSGI_APPLICATION = "ecommerce_shop_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "ecommerceDB",
+        "NAME": "ecommerce_shop",
         "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "localhost",
         "PORT": "5432",
     }
-}
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
