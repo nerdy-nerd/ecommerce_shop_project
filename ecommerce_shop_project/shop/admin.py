1370d4f1a84e7ed7323f4186bf6026f29433abce
Gosia <malgorzatastaniszewska@stxnext.com>
2019-05-19 20:55:35 +0200

    changes after CR

--- ecommerce_shop_project/shop/admin.py
+++ ecommerce_shop_project/shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Product, Category, Comment, Rating, Like
+from .models import Product, Category, Comment, Rating, Like, ProductImage
+
 
 admin.site.register(Product)
 admin.site.register(ProductImage)
