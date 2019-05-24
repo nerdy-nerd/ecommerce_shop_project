92c6155d5d8c3f3c81e0e4f66f4e352faed2b60b
MiraKaminska <32014509+MiraKaminska@users.noreply.github.com>
2019-05-24 18:57:16 +0300

    Ajax stars (#142)
    * connection to backend
    
    * stars are working :fire:

--- ecommerce_shop_project/shop/forms.py
+++ ecommerce_shop_project/shop/forms.py
@@ -6,7 +6,3 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["body"]
-
-
-class RatingForm(forms.Form):
-    rating = forms.IntegerField(max_value=5, min_value=1, widget=forms.HiddenInput)
--- ecommerce_shop_project/shop/static/js/stars.js
+++ ecommerce_shop_project/shop/static/js/stars.js
@@ -1,46 +1,58 @@
-//$(".ratings" ).on('click', function(event){
-//    event.preventDefault();
-//    var element = $(this);
-//    var rating = rating;
-//
-//    $.ajax({
-//        url : '/add_rating/'+$(this).attr(".ratings>span"),
-//        data: {rating: rating},
-//        type : 'POST',
-//
-//        success : function(data){
+$(".clickable > span" ).on('click', function(event){
+    event.preventDefault();
+    var element = $(this);
+
+    $.ajax({
+        url : '/add_rating/' + $(this).attr("data-id") + '/' + $(this).attr("data-star"),
+//        data: {data_star: data_star},
+        type : 'POST',
+
+        success : function(data){
+            const spans = document.querySelectorAll(".ratings > span");
+            spans.forEach(s => {
+                console.log(s);
+                s.classList.remove("empty-star");
+                s.classList.remove("text-dark");
+                s.classList.remove("hover");
+            });
+            const not_rated = document.querySelector(".not_rated");
+            if (not_rated){
+                not_rated.remove();
+            }
+            Array.from(spans).slice(data.stars).forEach(s => s.classList.add("empty-star"));
+            console.log(data.stars);
 //            element.html(' ' + data);
-//        }
-//    });
-//});
-//
-//
-//// You need these methods to add the CSRF token using jQuery
-//function getCookie(name) {
-//    var cookieValue = null;
-//    if (document.cookie && document.cookie !== '') {
-//        var cookies = document.cookie.split(';');
-//        for (var i = 0; i < cookies.length; i++) {
-//            var cookie = cookies[i].trim();
-//            // Does this cookie string begin with the name we want?
-//            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-//                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-//                break;
-//            }
-//        }
-//    }
-//    return cookieValue;
-//}
-//var csrftoken = getCookie('csrftoken')
-//
-//function csrfSafeMethod(method) {
-//    // these HTTP methods do not require CSRF protection
-//    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-//}
-//$.ajaxSetup({
-//    beforeSend: function(xhr, settings) {
-//        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-//            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-//        }
-//    }
-//});
+        }
+    });
+});
+
+
+// You need these methods to add the CSRF token using jQuery
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken')
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
